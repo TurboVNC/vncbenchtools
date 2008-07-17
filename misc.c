5717392b8692e82252819b5ac750a2b34ea43ae0
DRC <dcommander@users.sourceforge.net>
2008-07-17 18:13:00 +0000

    Decompression tests, pixel translation support
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/vncbenchtools/trunk@681 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- misc.c
+++ misc.c
@@ -1,10 +1,31 @@
+/*  Copyright (C) 2000 Const Kaplinsky <const@ce.cctpu.edu.ru>
+ *  Copyright (C) 2008 Sun Microsystems, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 /* misc functions */
 
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "rfb.h"
 
-char updateBuf[UPDATE_BUF_SIZE];
-int ublen;
+char updateBuf[UPDATE_BUF_SIZE], *sendBuf=NULL;
+int ublen, sblen=0, sbptr=0;
 
 rfbClientRec rfbClient;
 rfbScreenInfo rfbScreen;
@@ -14,7 +35,6 @@ void InitEverything (int color_depth)
 {
   int i;
 
-  rfbClient.translateFn = rfbTranslateNone;
   for (i = 0; i < MAX_ENCODINGS; i++) {
     rfbClient.rfbRectanglesSent[i] = 0;
     rfbClient.rfbBytesSent[i] = 0;
@@ -34,6 +54,12 @@ void InitEverything (int color_depth)
 
   rfbServerFormat.depth = color_depth;
   rfbServerFormat.bitsPerPixel = rfbClient.format.bitsPerPixel;
+  #ifdef _BIG_ENDIAN
+  rfbServerFormat.bigEndian = 1;
+  #else
+  rfbServerFormat.bigEndian = 0; 
+  #endif
+  rfbServerFormat.trueColour = 1;
   rfbScreen.bitsPerPixel = rfbClient.format.bitsPerPixel;
 
   switch (color_depth) {
@@ -61,37 +87,71 @@ void InitEverything (int color_depth)
     rfbClient.format.greenShift = 8;
     rfbClient.format.blueShift = 0;
   }
+  rfbClient.format.bigEndian = 0;
+  rfbClient.format.trueColour = 1;
+
+  rfbServerFormat.redMax = rfbClient.format.redMax;
+  rfbServerFormat.greenMax = rfbClient.format.greenMax;
+  rfbServerFormat.blueMax = rfbClient.format.blueMax;
+  rfbServerFormat.redShift = rfbClient.format.redShift;
+  rfbServerFormat.greenShift = rfbClient.format.greenShift;
+  rfbServerFormat.blueShift = rfbClient.format.blueShift;
+  if (rfbServerFormat.bigEndian) {
+     rfbServerFormat.redShift = rfbServerFormat.bitsPerPixel - 8 -
+                                (rfbServerFormat.redShift & (~7)) +
+                                (rfbServerFormat.redShift & 7);
+     rfbServerFormat.greenShift = rfbServerFormat.bitsPerPixel - 8 -
+                                  (rfbServerFormat.greenShift & (~7)) +
+                                  (rfbServerFormat.greenShift & 7);
+     rfbServerFormat.blueShift = rfbServerFormat.bitsPerPixel - 8 -
+                                 (rfbServerFormat.blueShift & (~7)) +
+                                 (rfbServerFormat.blueShift & 7);
+  }
+
+  rfbSetTranslateFunction(&rfbClient);
+
+  sendBuf = (char *)malloc(SEND_BUF_SIZE);
+  if (!sendBuf) {
+    printf("ERROR: Could not allocate send buffer.\n");
+    exit(1);
+  }
 
   ublen = 0;
 }
 
-/*
- * rfbTranslateNone is used when no translation is required.
- */
-
-void
-rfbTranslateNone(char *table, rfbPixelFormat *in, rfbPixelFormat *out,
-		 char *iptr, char *optr, int bytesBetweenInputLines,
-		 int width, int height)
-{
-    int bytesPerOutputLine = width * (out->bitsPerPixel / 8);
-
-    while (height > 0) {
-	memcpy(optr, iptr, bytesPerOutputLine);
-	iptr += bytesBetweenInputLines;
-	optr += bytesPerOutputLine;
-	height--;
-    }
-}
+extern int decompress;
 
 BOOL rfbSendUpdateBuf(rfbClientPtr cl)
 {
+  if(decompress) {
+    if (sblen + ublen > SEND_BUF_SIZE) {
+      printf("ERROR: Send buffer overrun.\n");
+      return False;
+    }
+    memcpy(&sendBuf[sblen], updateBuf, ublen);
+    sblen += ublen;
+  }
   ublen = 0;
   return TRUE;
 }
 
+Bool
+ReadFromRFBServer(char *out, unsigned int n)
+{
+  if (sbptr + n > SEND_BUF_SIZE) {
+    printf("ERROR: Send buffer overrun. %d %d %d\n", sbptr, n, SEND_BUF_SIZE);
+    return False;
+  }
+  memcpy(out, &sendBuf[sbptr], n);
+  sbptr += n;
+  return True;
+};
+
 int rfbLog (char *fmt, ...)
 {
+  va_list arglist;
+  va_start(arglist, fmt);
+  printf(fmt, arglist);
   return 0;
 }
 
