5ef478b7c19ebba79d777ccbc436bf2386afcbd8
DRC <dcommander@users.sourceforge.net>
2010-02-05 02:23:28 +0000

    If shutdown has occurred, exit before trying to call rfbSendUpdateBuf() (which w ill crash because the socket has already been killed)
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/vncbenchtools/trunk@957 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- turbo-0.7.c
+++ turbo-0.7.c
@@ -402,6 +402,7 @@ rfbSendRectEncodingTight(cl, x, y, w, h)
             pthread_mutex_lock (&tparam[i].done);
             status &= tparam[i].status;
         }
+        if (status == FALSE) return FALSE;
         if (ublen > 0) {
             if (!rfbSendUpdateBuf(cl))
                 return FALSE;
