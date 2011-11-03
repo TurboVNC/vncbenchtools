4bec781da5885aa161ae924efc2d34f7bebc131f
DRC <dcommander@users.sourceforge.net>
2011-11-03 00:27:58 +0000

    Compiler warnings
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/vncbenchtools/trunk@1654 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- hextile.c
+++ hextile.c
@@ -107,7 +107,7 @@ sendHextiles##bpp(cl, rx, ry, rw, rh)					      \
     int x, y, w, h;							      \
     int startUblen;							      \
     char *fbptr;							      \
-    CARD##bpp bg, fg, newBg, newFg;					      \
+    CARD##bpp bg = 0, fg = 0, newBg, newFg;				      \
     Bool mono, solid;							      \
     Bool validBg = FALSE;						      \
     Bool validFg = FALSE;						      \
@@ -302,7 +302,7 @@ testColours##bpp(data,size,mono,solid,bg,fg)				      \
     CARD##bpp *bg;							      \
     CARD##bpp *fg;							      \
 {									      \
-    CARD##bpp colour1, colour2;						      \
+    CARD##bpp colour1 = 0, colour2 = 0;					      \
     int n1 = 0, n2 = 0;							      \
     *mono = TRUE;							      \
     *solid = TRUE;							      \
