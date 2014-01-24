546c503b6df71ef471578e7e45ba54108dc2e4f0
DRC <dcommander@users.sourceforge.net>
2014-01-24 17:10:38 +0000

    Oops.  Make compression level 9 actually do something.
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/vncbenchtools/trunk@2719 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- turbo-1.1.c
+++ turbo-1.1.c
@@ -361,6 +361,14 @@ rfbSendRectEncodingTight(cl, x, y, w, h)
         if (!threadInit) return FALSE;
     }
 
+    /* CL 9 (which maps internally to CL 3) is included mainly for backward
+       compatibility with TightVNC Compression Levels 5-9.  It should be used
+       only in extremely low-bandwidth cases in which it can be shown to have a
+       benefit.  For low-color workloads, it provides typically only 10-20%
+       better compression than CL 2 with JPEG and CL 1 without JPEG, and it
+       uses, on average, twice as much CPU time. */
+    if (compressLevel > 3) compressLevel = 3;
+
     /* We only allow compression levels that have a demonstrable performance
        benefit.  CL 0 with JPEG reduces CPU usage for workloads that have low
        numbers of unique colors, but the same thing can be accomplished by
@@ -372,20 +380,11 @@ rfbSendRectEncodingTight(cl, x, y, w, h)
        significant reduction in bandwidth. */
     if (qualityLevel != -1) {
         if (compressLevel < 1) compressLevel = 1;
-        if (compressLevel > 2) compressLevel = 2;
     }
 
     /* With JPEG disabled, CL 2 offers no significant bandwidth savings over
        CL 1, so we don't include it. */
-    else if (compressLevel > 1) compressLevel = 1;
-
-    /* CL 9 (which maps internally to CL 3) is included mainly for backward
-       compatibility with TightVNC Compression Levels 5-9.  It should be used
-       only in extremely low-bandwidth cases in which it can be shown to have a
-       benefit.  For low-color workloads, it provides typically only 10-20%
-       better compression than CL 2 with JPEG and CL 1 without JPEG, and it
-       uses, on average, twice as much CPU time. */
-    if (compressLevel == 9) compressLevel = 3;
+    else if (compressLevel == 2) compressLevel = 1;
 
     if (cl->format.depth == 24 && cl->format.redMax == 0xFF &&
         cl->format.greenMax == 0xFF && cl->format.blueMax == 0xFF) {
